3904eca962aec26a2fcdc0241c3164d234d22739
venkataramanab <venkat.ramana295@gmail.com>
2020-01-22 10:00:38 +0530

    added babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
   entry: {
     upnext: './src/js/index.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   externals: {
     'video.js': 'videojs'
   },
